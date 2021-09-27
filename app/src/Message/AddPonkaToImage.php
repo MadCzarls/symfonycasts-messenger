9653d0ec4844f2fd5e5605efdd9ffd357a415cd7
Karol Kasiarz <k.kasiarz@andersenlab.com>
2021-09-27 14:32:04 +0200

    chapter 6, 7, 8

--- app/src/Message/AddPonkaToImage.php
+++ app/src/Message/AddPonkaToImage.php
@@ -4,19 +4,17 @@ declare(strict_types=1);
 
 namespace App\Message;
 
-use App\Entity\ImagePost;
-
 class AddPonkaToImage
 {
-    private ImagePost $imagePost;
+    private int $imagePostId;
 
-    public function __construct(ImagePost $imagePost)
+    public function __construct(int $imagePostId)
     {
-        $this->imagePost = $imagePost;
+        $this->imagePostId = $imagePostId;
     }
 
-    public function getImagePost(): ImagePost
+    public function getImagePostId(): int
     {
-        return $this->imagePost;
+        return $this->imagePostId;
     }
 }
