6b870c3da7b42c86d9e5464606ed1f8f9d51d1c9
MadCzarls <16895921+MadCzarls@users.noreply.github.com>
2021-09-27 11:24:36 +0200

    chapter 3

--- app/src/Message/AddPonkaToImage.php
+++ app/src/Message/AddPonkaToImage.php
@@ -4,7 +4,19 @@ declare(strict_types=1);
 
 namespace App\Message;
 
+use App\Entity\ImagePost;
+
 class AddPonkaToImage
 {
+    private ImagePost $imagePost;
 
+    public function __construct(ImagePost $imagePost)
+    {
+        $this->imagePost = $imagePost;
+    }
+
+    public function getImagePost(): ImagePost
+    {
+        return $this->imagePost;
+    }
 }
