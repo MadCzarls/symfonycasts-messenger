05692d9b8e88d895b4d4b9540316a0ce8cad2f53
MadCzarls <16895921+MadCzarls@users.noreply.github.com>
2021-10-13 12:22:55 +0200

    chapter 18

--- app/src/Middleware/AuditMiddleware.php
+++ app/src/Middleware/AuditMiddleware.php
@@ -5,12 +5,22 @@ declare(strict_types=1);
 namespace App\Middleware;
 
 use App\Stamp\UniqueIdStamp;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
 use Symfony\Component\Messenger\Middleware\StackInterface;
 
+use function get_class;
+
 class AuditMiddleware implements MiddlewareInterface
 {
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $messengerAuditLogger)
+    {
+        $this->logger = $messengerAuditLogger;
+    }
+
     public function handle(Envelope $envelope, StackInterface $stack): Envelope
     {
         if ($envelope->last(UniqueIdStamp::class) === null) {
@@ -19,7 +29,13 @@ class AuditMiddleware implements MiddlewareInterface
 
         /** @var UniqueIdStamp $stamp */
         $stamp = $envelope->last(UniqueIdStamp::class);
-        dump($stamp->getUniqueId());
+
+        $context = [
+            'id' => $stamp->getUniqueId(),
+            'class' => get_class($envelope->getMessage()),
+        ];
+
+        //@TODO log message but only on dispatching, not retrieving from transport (middlewares are triggered in both situations)
 
         return $stack->next()->handle($envelope, $stack);
     }
