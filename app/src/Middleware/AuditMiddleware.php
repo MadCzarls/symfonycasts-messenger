74b192e56692634c1dba70dc8654a88db05cd761
Karol Kasiarz <k.kasiarz@andersenlab.com>
2021-10-13 11:55:59 +0200

    chapter 17

--- app/src/Middleware/AuditMiddleware.php
+++ app/src/Middleware/AuditMiddleware.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Middleware;
 
+use App\Stamp\UniqueIdStamp;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
 use Symfony\Component\Messenger\Middleware\StackInterface;
@@ -12,7 +13,14 @@ class AuditMiddleware implements MiddlewareInterface
 {
     public function handle(Envelope $envelope, StackInterface $stack): Envelope
     {
-        //@TODO
+        if ($envelope->last(UniqueIdStamp::class) === null) {
+            $envelope = $envelope->with(new UniqueIdStamp());
+        }
+
+        /** @var UniqueIdStamp $stamp */
+        $stamp = $envelope->last(UniqueIdStamp::class);
+        dump($stamp->getUniqueId());
+
         return $stack->next()->handle($envelope, $stack);
     }
 }
