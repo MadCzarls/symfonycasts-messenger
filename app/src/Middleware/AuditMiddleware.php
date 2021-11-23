1ac8cb7601206772365d024e00cfbff718e68751
MadCzarls <16895921+MadCzarls@users.noreply.github.com>
2021-11-23 16:46:21 +0100

    AuditMiddleware comments and logs are now more precised

--- app/src/Middleware/AuditMiddleware.php
+++ app/src/Middleware/AuditMiddleware.php
@@ -39,11 +39,14 @@ class AuditMiddleware implements MiddlewareInterface
 
         $envelope = $stack->next()->handle($envelope, $stack);
 
-        if ($envelope->last(ReceivedStamp::class)) {
-            $this->logger->info('[{id}] Received from transport {class}', $context);
-        } elseif ($envelope->last(SentStamp::class)) {
+        if ($envelope->last(SentStamp::class)) {
+            //SentStamp means asynchronous handling - and that message is being sent to (to be later received and handled in) transport
             $this->logger->info('[{id}] Sent to transport {class}', $context);
-        } else { //no stamps mean synchronous handling (DeleteImagePostHandler) - no routing (transport) is used
+        } elseif ($envelope->last(ReceivedStamp::class)) {
+            //ReceivedStamp means asynchronous handling - and that message is being received from (after previously sent to) transport
+            $this->logger->info('[{id}] Received from transport {class}', $context);
+        } else {
+            //no stamps mean synchronous handling (DeleteImagePostHandler) - no routing (transport) is used
             $this->logger->info('[{id}] Handling sync {class}', $context);
         }
 
