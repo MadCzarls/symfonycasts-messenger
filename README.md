be3c67e91227357eb7fad3415af89ce3aac5dfa4
MadCzarls <16895921+MadCzarls@users.noreply.github.com>
2024-06-16 11:16:09 +0200

    feat: Review chapters: 32

--- README.md
+++ README.md
@@ -37,7 +37,7 @@ Sandboxes in previous Symfony versions are available at `legacy/symfony<VERSION>
 - [x] Chapter 29
 - [x] Chapter 30
 - [x] Chapter 31
-- [ ] Chapter 32
+- [x] Chapter 32
 - [ ] Chapter 33
 - [ ] Chapter 34
 - [ ] Chapter 35
--- app/src/MessageHandler/Query/GetTotalImageCountHandler.php
+++ app/src/MessageHandler/Query/GetTotalImageCountHandler.php
@@ -5,14 +5,19 @@ declare(strict_types=1);
 namespace App\MessageHandler\Query;
 
 use App\Message\Query\GetTotalImageCount;
+use App\Repository\ImagePostRepository;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
-class GetTotalImageCountHandler
+readonly class GetTotalImageCountHandler
 {
+    public function __construct(
+        private ImagePostRepository $imagePostRepository,
+    ) {
+    }
+
     public function __invoke(GetTotalImageCount $getTotalImageCount): int
     {
-        //@TODO
-        return 50;
+        return $this->imagePostRepository->count([]);
     }
 }
